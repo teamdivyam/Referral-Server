e52963c150971c8d68566bdda609a16580277195
Adesh Singh <adeshsingh3002@gmail.com>
2025-04-30 12:53:01 +0530

    .

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { PORT} from "./utils/constant.js";
 
 connectDB();
 
+// App Server Instance Running on Port: 3000
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
 });
