380d242c79ab3f106db57a0d36f86cbeef33d14a
Adesh Singh <adeshsingh3002@gmail.com>
2025-05-26 15:48:34 +0530

    refactor: implement validation for admin API

---
+++ src/api/validators/admin.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
+export const validatePageLimitSearch = Joi.object({
+    page: Joi.number(),
+    limit: Joi.number(),
+});
+
+export const objectIdValidation = (id) => {
+    return mongoose.Types.ObjectId.isValid(id);
+};
+
+export const assingReferralCodeQuantity = Joi.number();
+
+export const processWithdrawalValidation = Joi.object({
+    processType: Joi.string().valid("approved", "rejected"),
+    remarks: Joi.string().allow(null),
+});
+
+export const agentAccountStatus = Joi.string().valid(
+    "activate",
+    "deactivate"
+);
